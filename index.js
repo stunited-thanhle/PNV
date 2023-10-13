c4c4771f71019372b88d0628f82750a8ed74dde6
Thanh Le <thanh.le@stunited.vn>
2023-10-13 09:53:48 +0700

    add cloudinary

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const fetchProducts = async () => {
-  return await axios.get("http://localhost:4001/posts");
+  return await axios.get("https://pnv-team.onrender.com/posts");
 };
 
 (async () => {
@@ -55,3 +55,40 @@ const fetchProducts = async () => {
     })
     .join("");
 })();
+
+const fileInput = document.getElementById("fileInput");
+const uploading_text = document.getElementById("uploading_text");
+
+// replace with your data 👇
+const cloud_name = "dvjyivpcf";
+const upload_preset = "uploadpnv";
+// replace with your data 👆
+
+fileInput.addEventListener("change", (e) => {
+  uploading_text.innerText = "uploading...";
+  const file = e.target.files[0];
+  const formData = new FormData();
+  formData.append("file", file);
+  formData.append("upload_preset", upload_preset);
+  const options = {
+    method: "POST",
+    body: formData,
+  };
+
+  return fetch(
+    `https://api.cloudinary.com/v1_1/${cloud_name}/image/upload`,
+    options
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data.secure_url);
+
+      uploading_text.innerHTML = `
+      <span>upload complete.</span>
+      <br />
+      <img style="max-width:300px" src="${data.secure_url}" alt="">
+      <a href="${data.secure_url}">${data.secure_url}</a>
+      `;
+    })
+    .catch((err) => console.log(err));
+});
